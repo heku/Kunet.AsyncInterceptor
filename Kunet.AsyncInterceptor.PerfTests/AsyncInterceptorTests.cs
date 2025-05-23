10ea77376ef9fdbfd0f817c3c5596480d34465e8
Heku <heku.git@outlook.com>
2025-05-24 00:15:04 +0800

    Add benchmark to readme

--- Kunet.AsyncInterceptor.PerfTests/AsyncInterceptorTests.cs
+++ Kunet.AsyncInterceptor.PerfTests/AsyncInterceptorTests.cs
@@ -1,12 +1,15 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using Castle.DynamicProxy;
 
 namespace Kunet.AsyncInterceptor.PerfTests;
 
-[ShortRunJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net481)]
-[ShortRunJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net80)]
+[ShortRunJob(RuntimeMoniker.Net481)]
+[ShortRunJob(RuntimeMoniker.Net80)]
 [MemoryDiagnoser, CategoriesColumn]
-[HideColumns("Error", "StdDev", "Medium")]
+[HideColumns("Error", "StdDev", "Medium", "RatioSD", "Job")]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByJob, BenchmarkLogicalGroupRule.ByCategory)]
 public class AsyncInterceptorTests
 {
     public IFoo StakxProxy { get; set; }
@@ -25,7 +28,7 @@ public class AsyncInterceptorTests
     [BenchmarkCategory("Void")]
     public void StakxVoid() => StakxProxy.VoidMethod();
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("Void")]
     public void KunetVoid() => KunetProxy.VoidMethod();
 
@@ -33,7 +36,7 @@ public class AsyncInterceptorTests
     [BenchmarkCategory("Task")]
     public Task StakxTask() => StakxProxy.TaskMethod();
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("Task")]
     public Task KunetTask() => KunetProxy.TaskMethod();
 
@@ -41,7 +44,7 @@ public class AsyncInterceptorTests
     [BenchmarkCategory("Task<T>")]
     public Task<int> StakxTaskT() => StakxProxy.TaskMethod<int>();
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("Task<T>")]
     public Task<int> KunetTaskT() => KunetProxy.TaskMethod<int>();
 
@@ -55,7 +58,7 @@ public class AsyncInterceptorTests
         return task;
     }
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("RunningTask<T>")]
     public Task<int> KunetRunningTaskT()
     {
@@ -69,7 +72,7 @@ public class AsyncInterceptorTests
     [BenchmarkCategory("ValueTask")]
     public ValueTask StakxValueTask() => StakxProxy.ValueTaskMethod();
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("ValueTask")]
     public ValueTask KunetValueTask() => KunetProxy.ValueTaskMethod();
 
@@ -77,7 +80,7 @@ public class AsyncInterceptorTests
     [BenchmarkCategory("ValueTask<T>")]
     public ValueTask<int> StakxValueTaskT() => StakxProxy.ValueTaskMethod<int>();
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("ValueTask<T>")]
     public ValueTask<int> KunetValueTaskT() => KunetProxy.ValueTaskMethod<int>();
 
@@ -91,7 +94,7 @@ public class AsyncInterceptorTests
         return task;
     }
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     [BenchmarkCategory("RunningValueTask<T>")]
     public ValueTask<int> KunetRunningValueTaskT()
     {
@@ -101,36 +104,3 @@ public class AsyncInterceptorTests
         return task;
     }
 }
-
-
-
-//| Method                 | Job                           | Runtime              | Categories          | Mean        | Gen0   | Allocated |
-//|----------------------- |------------------------------ |--------------------- |-------------------- |------------:|-------:|----------:|
-//| StakxRunningTaskT      | ShortRun-.NET 8.0             | .NET 8.0             | RunningTask<T>      | 1,988.02 ns | 0.1221 |    2104 B |
-//| KunetRunningTaskT      | ShortRun-.NET 8.0             | .NET 8.0             | RunningTask<T>      |   304.62 ns | 0.0539 |     904 B |
-//| StakxRunningTaskT      | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | RunningTask<T>      | 6,746.47 ns | 0.5493 |    3490 B |
-//| KunetRunningTaskT      | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | RunningTask<T>      | 1,191.40 ns | 0.2537 |    1597 B |
-//| StakxRunningValueTaskT | ShortRun-.NET 8.0             | .NET 8.0             | RunningValueTask<T> | 2,847.12 ns | 0.1450 |    2440 B |
-//| KunetRunningValueTaskT | ShortRun-.NET 8.0             | .NET 8.0             | RunningValueTask<T> |   314.22 ns | 0.0587 |     984 B |
-//| StakxRunningValueTaskT | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | RunningValueTask<T> | 7,112.27 ns | 0.5951 |    3755 B |
-//| KunetRunningValueTaskT | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | RunningValueTask<T> | 1,230.46 ns | 0.2651 |    1677 B |
-//| StakxTask              | ShortRun-.NET 8.0             | .NET 8.0             | Task                |   880.12 ns | 0.0448 |     752 B |
-//| KunetTask              | ShortRun-.NET 8.0             | .NET 8.0             | Task                |    28.58 ns | 0.0110 |     184 B |
-//| StakxTask              | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Task                | 3,242.71 ns | 0.1793 |    1140 B |
-//| KunetTask              | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Task                |    45.88 ns | 0.0293 |     185 B |
-//| StakxTaskT             | ShortRun-.NET 8.0             | .NET 8.0             | Task<T>             | 1,304.30 ns | 0.0668 |    1144 B |
-//| KunetTaskT             | ShortRun-.NET 8.0             | .NET 8.0             | Task<T>             |   133.39 ns | 0.0262 |     440 B |
-//| StakxTaskT             | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Task<T>             | 4,425.31 ns | 0.3281 |    2071 B |
-//| KunetTaskT             | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Task<T>             |   490.25 ns | 0.1249 |     786 B |
-//| StakxValueTask         | ShortRun-.NET 8.0             | .NET 8.0             | ValueTask           | 1,671.27 ns | 0.0629 |    1080 B |
-//| KunetValueTask         | ShortRun-.NET 8.0             | .NET 8.0             | ValueTask           |    37.36 ns | 0.0148 |     248 B |
-//| StakxValueTask         | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | ValueTask           | 3,627.59 ns | 0.2022 |    1276 B |
-//| KunetValueTask         | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | ValueTask           |    53.79 ns | 0.0395 |     249 B |
-//| StakxValueTaskT        | ShortRun-.NET 8.0             | .NET 8.0             | ValueTask<T>        | 2,096.30 ns | 0.0877 |    1472 B |
-//| KunetValueTaskT        | ShortRun-.NET 8.0             | .NET 8.0             | ValueTask<T>        |   144.43 ns | 0.0300 |     504 B |
-//| StakxValueTaskT        | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | ValueTask<T>        | 4,605.92 ns | 0.3204 |    2055 B |
-//| KunetValueTaskT        | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | ValueTask<T>        |   499.61 ns | 0.1221 |     770 B |
-//| StakxVoid              | ShortRun-.NET 8.0             | .NET 8.0             | Void                |    75.74 ns | 0.0062 |     104 B |
-//| KunetVoid              | ShortRun-.NET 8.0             | .NET 8.0             | Void                |    18.77 ns | 0.0062 |     104 B |
-//| StakxVoid              | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Void                | 1,089.48 ns | 0.0515 |     329 B |
-//| KunetVoid              | ShortRun-.NET Framework 4.8.1 | .NET Framework 4.8.1 | Void                |    33.64 ns | 0.0166 |     104 B |
--- README.md
+++ README.md
@@ -28,3 +28,55 @@ class Example : AsyncInterceptor
     }
 }
 ```
+
+#### Benchmark
+
+##### .NET 8.0
+
+| Method                 | Categories          | Mean        | Ratio | Gen0   | Allocated | Alloc Ratio |
+|----------------------- |-------------------- |------------:|------:|-------:|----------:|------------:|
+| StakxRunningTaskT      | RunningTask&lt;T&gt;      | 2,076.89 ns |  6.64 | 0.1221 |    2104 B |        2.33 |
+| KunetRunningTaskT      | RunningTask&lt;T&gt;      |   312.92 ns |  1.00 | 0.0539 |     904 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxRunningValueTaskT | RunningValueTask&lt;T&gt; | 2,908.91 ns |  8.54 | 0.1450 |    2440 B |        2.48 |
+| KunetRunningValueTaskT | RunningValueTask&lt;T&gt; |   340.72 ns |  1.00 | 0.0587 |     984 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxTask              | Task                |   916.48 ns | 31.27 | 0.0448 |     752 B |        4.09 |
+| KunetTask              | Task                |    29.31 ns |  1.00 | 0.0110 |     184 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxTaskT             | Task&lt;T&gt;             | 1,303.35 ns |  9.44 | 0.0648 |    1144 B |        2.60 |
+| KunetTaskT             | Task&lt;T&gt;             |   138.14 ns |  1.00 | 0.0262 |     440 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxValueTask         | ValueTask           | 1,713.29 ns | 45.55 | 0.0629 |    1080 B |        4.35 |
+| KunetValueTask         | ValueTask           |    37.62 ns |  1.00 | 0.0148 |     248 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxValueTaskT        | ValueTask&lt;T&gt;        | 2,118.77 ns | 14.53 | 0.0877 |    1472 B |        2.92 |
+| KunetValueTaskT        | ValueTask&lt;T&gt;        |   145.85 ns |  1.00 | 0.0300 |     504 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxVoid              | Void                |    77.71 ns |  4.14 | 0.0062 |     104 B |        1.00 |
+| KunetVoid              | Void                |    18.78 ns |  1.00 | 0.0062 |     104 B |        1.00 |
+
+##### .NET Framework 4.8.1
+
+| Method                 | Categories          | Mean        | Ratio | Gen0   | Allocated | Alloc Ratio |
+|----------------------- |-------------------- |------------:|------:|-------:|----------:|------------:|
+| StakxRunningTaskT      | RunningTask&lt;T&gt;      | 6,797.09 ns |  5.59 | 0.5798 |    3684 B |        2.31 |
+| KunetRunningTaskT      | RunningTask&lt;T&gt;      | 1,215.56 ns |  1.00 | 0.2537 |    1597 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxRunningValueTaskT | RunningValueTask&lt;T&gt; | 7,117.17 ns |  5.67 | 0.5951 |    3755 B |        2.24 |
+| KunetRunningValueTaskT | RunningValueTask&lt;T&gt; | 1,254.33 ns |  1.00 | 0.2651 |    1677 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxTask              | Task                | 3,158.44 ns | 65.02 | 0.1793 |    1140 B |        6.16 |
+| KunetTask              | Task                |    48.58 ns |  1.00 | 0.0293 |     185 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxTaskT             | Task&lt;T&gt;             | 4,347.93 ns |  8.71 | 0.3281 |    2071 B |        2.63 |
+| KunetTaskT             | Task&lt;T&gt;             |   499.03 ns |  1.00 | 0.1249 |     786 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxValueTask         | ValueTask           | 3,612.61 ns | 67.68 | 0.2022 |    1276 B |        5.12 |
+| KunetValueTask         | ValueTask           |    53.37 ns |  1.00 | 0.0395 |     249 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxValueTaskT        | ValueTask&lt;T&gt;        | 4,596.25 ns |  8.52 | 0.3204 |    2055 B |        2.67 |
+| KunetValueTaskT        | ValueTask&lt;T&gt;        |   539.78 ns |  1.00 | 0.1221 |     770 B |        1.00 |
+|                        |                     |             |       |        |           |             |
+| StakxVoid              | Void                | 1,111.00 ns | 32.64 | 0.0515 |     329 B |        3.16 |
+| KunetVoid              | Void                |    34.04 ns |  1.00 | 0.0166 |     104 B |        1.00 |
